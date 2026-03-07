9ec993a97edc634fa674ba6d39eb9101d1e7ce3f
u006E10011 <zaaaaazzzazzaz@gmail.com>
2026-03-07 17:41:13 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg
+
+# Добавление официального GPG ключа Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Настройка репозитория
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+# Установка и запуск
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker compose up -d --build
+
+# Инициализация базы данных
+sudo docker compose exec web flask db init
+sudo docker compose exec web flask db migrate -m "Initial migration"
+sudo docker compose exec web flask db upgrade
